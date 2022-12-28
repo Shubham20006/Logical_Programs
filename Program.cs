a900531823153b761a8d6ff0154e15a2be02ccec
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-28 20:38:39 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+namespace Logical_Problem
+{
+    public class CallingMainMethod
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the logical program.");
+        }
+    }
+}
